09688f3f0a3e0ad8776a70324b00b6eb3fa8dbb1
Vlad6555 <Vlad6555@ukr.net>
2019-04-15 12:40:10 +0300

    add grid

--- src/App.js
+++ src/App.js
@@ -1,35 +1,74 @@
 import React from "react"
-import MyMapComponent from "./Components/maps/map";
+// import MyMapComponent from "./Components/maps/map";
 
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-class App extends React.Component {
-  render() {
-    return (
-      <React.Fragment>
-        <MyMapComponent />
-        <p>
-          Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
-          Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
-          В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов,
-          используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил
-          без заметных изменений пять веков, но и перешагнул в электронный дизайн.
-          Его популяризации в новое время послужили публикация листов Letraset с образцами
-          Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа
-          Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.
-      </p>
-        <p>
-          Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
-          Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
-          В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов,
-          используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил
-          без заметных изменений пять веков, но и перешагнул в электронный дизайн.
-          Его популяризации в новое время послужили публикация листов Letraset с образцами
-          Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа
-          Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.
-      </p>
-      </React.Fragment>
-    )
-  }
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+});
+
+function App(props) {
+  const { classes } = props;
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={24}>
+        <Grid item xs={12} md={6}>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+        <Grid item xs={12} md={6}>
+          <Paper className={classes.paper}>xs</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-export default App;
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
+
+
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <MyMapComponent />
+//         <p>
+//           Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+//           Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
+//           В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов,
+//           используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил
+//           без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+//           Его популяризации в новое время послужили публикация листов Letraset с образцами
+//           Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа
+//           Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.
+//       </p>
+//         <p>
+//           Lorem Ipsum - это текст-"рыба", часто используемый в печати и вэб-дизайне.
+//           Lorem Ipsum является стандартной "рыбой" для текстов на латинице с начала XVI века.
+//           В то время некий безымянный печатник создал большую коллекцию размеров и форм шрифтов,
+//           используя Lorem Ipsum для распечатки образцов. Lorem Ipsum не только успешно пережил
+//           без заметных изменений пять веков, но и перешагнул в электронный дизайн.
+//           Его популяризации в новое время послужили публикация листов Letraset с образцами
+//           Lorem Ipsum в 60-х годах и, в более недавнее время, программы электронной вёрстки типа
+//           Aldus PageMaker, в шаблонах которых используется Lorem Ipsum.
+//       </p>
+//       </React.Fragment>
+//     )
+//   }
+// }
+// export default App;
